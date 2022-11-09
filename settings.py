b310cc9c41240291a670e007f2b551581ed19ac3
osik1488 <109912047+osik1488@users.noreply.github.com>
2022-11-09 20:31:56 +0600

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+valid_email = os.getenv('valid_email')
+valid_password = os.getenv('valid_password')
+invalid_email="111"
+invalid_password=""
+infinit_password='1111111111111111111111111111111111111111111111111111111111111122222222222222222222222222222222222223'
